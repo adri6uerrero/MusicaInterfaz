ec54c6783ebc70acd7327629f3c5c34a647ac479
Adrian Guerrero <150003752+adri6uerrero@users.noreply.github.com>
2025-02-19 13:06:57 +0100

    Add files via upload

---
+++ MusicaImports/MusicaImports/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace MusicaImports
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.rjButton1 = new CustomControls.RJControls.RJButton();
+            this.rjButtonBack1 = new CustomControls.RJControls.RJButtonBack();
+            this.rjButtonFw1 = new CustomControls.RJControls.RJButtonFw();
+            this.tbDarkTheme = new CustomControls.RJControls.RJToggleButton();
+            this.rjButtonX1 = new CustomControls.RJControls.RJButtonX();
+            this.rjCircularPictureBox1 = new CustomControls.RJControls.RJCircularPictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.rjProgressBar1 = new CustomControls.RJControls.RJProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.rjCircularPictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rjButton1
+            // 
+            this.rjButton1.BackColor = System.Drawing.Color.Maroon;
+            this.rjButton1.BackgroundColor = System.Drawing.Color.Maroon;
+            this.rjButton1.BorderColor = System.Drawing.Color.MidnightBlue;
+            this.rjButton1.BorderRadius = 0;
+            this.rjButton1.BorderSize = 0;
+            this.rjButton1.FlatAppearance.BorderSize = 0;
+            this.rjButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rjButton1.ForeColor = System.Drawing.Color.White;
+            this.rjButton1.IconColor = System.Drawing.Color.White;
+            this.rjButton1.Location = new System.Drawing.Point(334, 315);
+            this.rjButton1.Name = "rjButton1";
+            this.rjButton1.Size = new System.Drawing.Size(42, 38);
+            this.rjButton1.TabIndex = 0;
+            this.rjButton1.TextColor = System.Drawing.Color.White;
+            this.rjButton1.UseVisualStyleBackColor = false;
+            this.rjButton1.Click += new System.EventHandler(this.rjButton1_Click);
+            // 
+            // rjButtonBack1
+            // 
+            this.rjButtonBack1.BackColor = System.Drawing.Color.Maroon;
+            this.rjButtonBack1.BackgroundColor = System.Drawing.Color.Maroon;
+            this.rjButtonBack1.BorderColor = System.Drawing.Color.MidnightBlue;
+            this.rjButtonBack1.BorderRadius = 0;
+            this.rjButtonBack1.BorderSize = 0;
+            this.rjButtonBack1.FlatAppearance.BorderSize = 0;
+            this.rjButtonBack1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rjButtonBack1.ForeColor = System.Drawing.Color.White;
+            this.rjButtonBack1.IconColor = System.Drawing.Color.White;
+            this.rjButtonBack1.Location = new System.Drawing.Point(400, 315);
+            this.rjButtonBack1.Name = "rjButtonBack1";
+            this.rjButtonBack1.Size = new System.Drawing.Size(44, 38);
+            this.rjButtonBack1.TabIndex = 1;
+            this.rjButtonBack1.TextColor = System.Drawing.Color.White;
+            this.rjButtonBack1.UseVisualStyleBackColor = false;
+            // 
+            // rjButtonFw1
+            // 
+            this.rjButtonFw1.BackColor = System.Drawing.Color.Maroon;
+            this.rjButtonFw1.BackgroundColor = System.Drawing.Color.Maroon;
+            this.rjButtonFw1.BorderColor = System.Drawing.Color.MidnightBlue;
+            this.rjButtonFw1.BorderRadius = 0;
+            this.rjButtonFw1.BorderSize = 0;
+            this.rjButtonFw1.FlatAppearance.BorderSize = 0;
+            this.rjButtonFw1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rjButtonFw1.ForeColor = System.Drawing.Color.White;
+            this.rjButtonFw1.IconColor = System.Drawing.Color.White;
+            this.rjButtonFw1.Location = new System.Drawing.Point(266, 315);
+            this.rjButtonFw1.Name = "rjButtonFw1";
+            this.rjButtonFw1.Size = new System.Drawing.Size(38, 38);
+            this.rjButtonFw1.TabIndex = 2;
+            this.rjButtonFw1.TextColor = System.Drawing.Color.White;
+            this.rjButtonFw1.UseVisualStyleBackColor = false;
+            this.rjButtonFw1.Click += new System.EventHandler(this.rjButtonFw1_Click);
+            // 
+            // tbDarkTheme
+            // 
+            this.tbDarkTheme.Location = new System.Drawing.Point(266, 44);
+            this.tbDarkTheme.MinimumSize = new System.Drawing.Size(45, 22);
+            this.tbDarkTheme.Name = "tbDarkTheme";
+            this.tbDarkTheme.OffBackColor = System.Drawing.Color.Maroon;
+            this.tbDarkTheme.OffToggleColor = System.Drawing.Color.Gainsboro;
+            this.tbDarkTheme.OnBackColor = System.Drawing.Color.Maroon;
+            this.tbDarkTheme.OnToggleColor = System.Drawing.Color.GhostWhite;
+            this.tbDarkTheme.Size = new System.Drawing.Size(45, 22);
+            this.tbDarkTheme.TabIndex = 0;
+            this.tbDarkTheme.UseVisualStyleBackColor = true;
+            this.tbDarkTheme.CheckedChanged += new System.EventHandler(this.tbDarkTheme_CheckedChanged_1);
+            // 
+            // rjButtonX1
+            // 
+            this.rjButtonX1.BackColor = System.Drawing.Color.Maroon;
+            this.rjButtonX1.BackgroundColor = System.Drawing.Color.Maroon;
+            this.rjButtonX1.BorderColor = System.Drawing.Color.MidnightBlue;
+            this.rjButtonX1.BorderRadius = 0;
+            this.rjButtonX1.BorderSize = 0;
+            this.rjButtonX1.FlatAppearance.BorderSize = 0;
+            this.rjButtonX1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rjButtonX1.ForeColor = System.Drawing.Color.White;
+            this.rjButtonX1.IconColor = System.Drawing.Color.White;
+            this.rjButtonX1.Location = new System.Drawing.Point(418, 44);
+            this.rjButtonX1.Name = "rjButtonX1";
+            this.rjButtonX1.Size = new System.Drawing.Size(26, 22);
+            this.rjButtonX1.TabIndex = 4;
+            this.rjButtonX1.TextColor = System.Drawing.Color.White;
+            this.rjButtonX1.UseVisualStyleBackColor = false;
+            this.rjButtonX1.Click += new System.EventHandler(this.rjButtonX1_Click);
+            // 
+            // rjCircularPictureBox1
+            // 
+            this.rjCircularPictureBox1.BorderCapStyle = System.Drawing.Drawing2D.DashCap.Round;
+            this.rjCircularPictureBox1.BorderColor = System.Drawing.Color.Maroon;
+            this.rjCircularPictureBox1.BorderColor2 = System.Drawing.Color.MidnightBlue;
+            this.rjCircularPictureBox1.BorderLineStyle = System.Drawing.Drawing2D.DashStyle.DashDotDot;
+            this.rjCircularPictureBox1.BorderSize = 10;
+            this.rjCircularPictureBox1.GradientAngle = 50F;
+            this.rjCircularPictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("rjCircularPictureBox1.Image")));
+            this.rjCircularPictureBox1.Location = new System.Drawing.Point(285, 84);
+            this.rjCircularPictureBox1.Name = "rjCircularPictureBox1";
+            this.rjCircularPictureBox1.Size = new System.Drawing.Size(132, 132);
+            this.rjCircularPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.rjCircularPictureBox1.TabIndex = 5;
+            this.rjCircularPictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(263, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Dark theme";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // rjProgressBar1
+            // 
+            this.rjProgressBar1.ChannelColor = System.Drawing.Color.LightSteelBlue;
+            this.rjProgressBar1.ChannelHeight = 6;
+            this.rjProgressBar1.ForeBackColor = System.Drawing.Color.Maroon;
+            this.rjProgressBar1.ForeColor = System.Drawing.Color.DarkGray;
+            this.rjProgressBar1.Location = new System.Drawing.Point(266, 275);
+            this.rjProgressBar1.Name = "rjProgressBar1";
+            this.rjProgressBar1.ShowMaximun = false;
+            this.rjProgressBar1.ShowValue = CustomControls.RJControls.TextPosition.Right;
+            this.rjProgressBar1.Size = new System.Drawing.Size(178, 23);
+            this.rjProgressBar1.SliderColor = System.Drawing.Color.Maroon;
+            this.rjProgressBar1.SliderHeight = 6;
+            this.rjProgressBar1.SymbolAfter = "";
+            this.rjProgressBar1.SymbolBefore = "";
+            this.rjProgressBar1.TabIndex = 7;
+            this.rjProgressBar1.Click += new System.EventHandler(this.rjProgressBar1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(339, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "2XP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(294, 243);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Sticky MA ft Leiti Sene";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rjProgressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rjCircularPictureBox1);
+            this.Controls.Add(this.rjButtonX1);
+            this.Controls.Add(this.tbDarkTheme);
+            this.Controls.Add(this.rjButtonFw1);
+            this.Controls.Add(this.rjButtonBack1);
+            this.Controls.Add(this.rjButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.rjCircularPictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CustomControls.RJControls.RJButton rjButton1;
+        private CustomControls.RJControls.RJButtonBack rjButtonBack1;
+        private CustomControls.RJControls.RJButtonFw rjButtonFw1;
+        private CustomControls.RJControls.RJToggleButton tbDarkTheme;
+        private CustomControls.RJControls.RJButtonX rjButtonX1;
+        private CustomControls.RJControls.RJCircularPictureBox rjCircularPictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private CustomControls.RJControls.RJProgressBar rjProgressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MusicaImports/MusicaImports/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CustomControls.RJControls;
+
+namespace MusicaImports
+{
+    public partial class Form1 : Form
+    {
+        private int segundosTranscurridos = 0;  
+        private int totalSegundos = 151;        
+
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Font = new Font(label2.Font, FontStyle.Bold);
+            label3.Font = new Font(label3.Font, FontStyle.Bold);
+
+        }
+        private void rjCircularPictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbDarkTheme_CheckedChanged_1(object sender, EventArgs e)
+        {
+            if (tbDarkTheme.Checked)
+            {
+                this.BackColor = Color.DimGray;
+                label1.ForeColor = Color.WhiteSmoke;
+            }
+            else
+            {
+                this.BackColor = Color.WhiteSmoke;
+                label1.ForeColor = Color.DimGray;
+            }
+        }
+
+        private bool enEjecucion = false; 
+
+        private void rjButton1_Click(object sender, EventArgs e)
+        {
+            if (enEjecucion)
+            {
+                timer1.Stop();
+                enEjecucion = false;
+            }
+            else
+            {
+                timer1.Start();
+                enEjecucion = true;
+            }
+        }
+
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (segundosTranscurridos < totalSegundos)
+            {
+                segundosTranscurridos++;
+                rjProgressBar1.Value = segundosTranscurridos;
+
+                rjProgressBar1.SymbolBefore = FormatearTiempo(segundosTranscurridos);
+                rjProgressBar1.SymbolAfter = " / " + FormatearTiempo(totalSegundos);
+            }
+            else
+            {
+                timer1.Stop(); 
+            }
+        }
+
+
+        private string FormatearTiempo(int segundos)
+        {
+            int minutos = segundos / 60;
+            int segundosRestantes = segundos % 60;
+            return $"{minutos:D2}:{segundosRestantes:D2}"; 
+            rjProgressBar1.SymbolBefore = $"{FormatearTiempo(segundosTranscurridos)} ";
+            rjProgressBar1.SymbolAfter = $" / {FormatearTiempo(totalSegundos)} ";
+
+        }
+
+
+
+        private void rjProgressBar1_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+            int nuevoValor = (int)((double)me.X / rjProgressBar1.Width * totalSegundos);
+
+            segundosTranscurridos = nuevoValor;
+            rjProgressBar1.Value = nuevoValor;
+
+            rjProgressBar1.SymbolBefore = FormatearTiempo(nuevoValor);
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rjProgressBar1.Minimum = 0;
+            rjProgressBar1.Maximum = totalSegundos; 
+            rjProgressBar1.ShowValue = TextPosition.Sliding; 
+            rjProgressBar1.SymbolBefore = FormatearTiempo(0); 
+            rjProgressBar1.SymbolAfter = " / " + FormatearTiempo(totalSegundos); 
+        }
+
+        private void rjButtonFw1_Click(object sender, EventArgs e)
+        {
+            rjProgressBar1.Value = 0;
+            segundosTranscurridos = 0; 
+        }
+
+        private void rjButtonX1_Click(object sender, EventArgs e)
+        {
+            this.Close(); 
+        }
+    }
+}
